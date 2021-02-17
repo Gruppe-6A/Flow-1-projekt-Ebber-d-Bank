2caf7e2afa93b0663f525e6fb8a7affa65946be1
mikkeg05 <36851768+mikkeg05@users.noreply.github.com>
2021-02-17 10:38:24 +0100

    17-02 morgen

--- src/main/java/Menu.java
+++ src/main/java/Menu.java
@@ -5,26 +5,49 @@ public class Menu {
     public Menu(){
 
     }
-
+    public int managementID = 10;
     Scanner scanner = new Scanner(System.in);
     Customer customer = new Customer("Lars");
     Account account = new Account(customer);
     public int amount;
-    public void onStartup(){
-        System.out.println("Indtast ID");
-        scanner.nextInt();
-        mainMenu();
+    public int onStartup(){
+        int ID;
+        System.out.println("Enter ID");
+        ID = scanner.nextInt();
+        if(scanner.nextInt() == managementID){
+            bankMenu();
+        } else {
+            mainMenu();
+        }
+        return ID;
     }
     private void mainMenu(){
-        System.out.println("1) Withdraw \n2) Deposit \n3) View bankstatement \n4) Transfer money");
+        System.out.println("1) Withdraw \n2) Deposit \n3) View bankstatement");
         choice();
+    }
+    private void bankMenu(){
+        System.out.println("1) View account \n 2) Transfer money");
+        managementChoice();
+    }
+
+    private void managementChoice(){
+        switch(scanner.nextInt()){
+            case 1:
+                System.out.println("Enter ID on the account you wish to view.");
+                //Indtast ID
+                break;
+            case 2:
+                System.out.println("Enter ID on the account you wish to transfer from");
+                //Indtast ID
+                break;
+        }
     }
 
     private void choice() {
 
         switch(scanner.nextInt()){
             case 1:
-                System.out.println("Hvor meget skal der hæves?");
+                System.out.println("How much do you wish to withdraw?");
                 amount = scanner.nextInt();
                 try {
                     account.withDrawAmount(amount);
@@ -35,7 +58,7 @@ public class Menu {
                 }
                 break;
             case 2:
-                System.out.println("Hvor meget skal der indsættes?");
+                System.out.println("How much do you wish to deposit?");
                 amount = scanner.nextInt();
                 try {
                     account.depositAmount(amount);
